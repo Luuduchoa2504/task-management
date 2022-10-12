3bf912b6a1506cd536d2e8f6e0f17ea34ecdb48d
hoa.luu-duc2 <hoa.luu-duc2@banvien.com.vn>
2022-10-12 11:57:45 +0700

    add authentication

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,12 @@
+import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.enity';
+
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+}
