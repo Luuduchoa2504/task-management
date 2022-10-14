1a600d7b7c412d2915065f2e1d36bb19c913b3d0
hoa luu <luuduchoa2504@gmail.com>
2022-10-14 15:42:30 +0700

    add logger configuration

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -4,8 +4,12 @@ import { Repository } from 'typeorm';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Logger } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common/exceptions';
 
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TasksRepository');
+
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
@@ -22,8 +26,18 @@ export class TasksRepository extends Repository<Task> {
       );
     }
 
-    const tasks = await query.getMany();
-    return tasks;
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (error) {
+      this.logger.error(
+        `Failed to get tasks for user "${
+          user.username
+        }". Filters: ${JSON.stringify(filterDto)}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
